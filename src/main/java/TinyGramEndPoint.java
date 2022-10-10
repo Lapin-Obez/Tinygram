e9371bfc59f4fc11f2fbc0cfa513493b854299ba
Tristan RAME <tristan.rame@etu.univ-nantes.fr>
2022-10-10 13:26:26 +0000

    creation arborescence

---
+++ src/main/java/TinyGramEndPoint.java
@@ -0,0 +1,41 @@
+import java.util.Date;
+import java.util.List;
+import java.util.Random;
+
+import com.google.api.server.spi.auth.common.User;
+import com.google.api.server.spi.config.Api;
+import com.google.api.server.spi.config.ApiMethod;
+import com.google.api.server.spi.config.ApiMethod.HttpMethod;
+import com.google.api.server.spi.config.ApiNamespace;
+import com.google.api.server.spi.config.Named;
+import com.google.api.server.spi.config.Nullable;
+import com.google.api.server.spi.response.CollectionResponse;
+import com.google.api.server.spi.response.UnauthorizedException;
+
+import com.google.appengine.api.datastore.Cursor;
+import com.google.appengine.api.datastore.DatastoreService;
+import com.google.appengine.api.datastore.DatastoreServiceFactory;
+import com.google.appengine.api.datastore.Entity;
+import com.google.appengine.api.datastore.FetchOptions;
+import com.google.appengine.api.datastore.Query;
+import com.google.appengine.api.datastore.PreparedQuery;
+import com.google.appengine.api.datastore.Query.FilterOperator;
+import com.google.appengine.api.datastore.Query.FilterPredicate;
+import com.google.appengine.api.datastore.Query.SortDirection;
+import com.google.appengine.api.datastore.QueryResultList;
+import com.google.appengine.api.datastore.Transaction;
+
+@Api(name = "myApi",
+     version = "v1",
+     audiences = "TODO",
+  	 clientIds = "TODO",
+     namespace =
+     @ApiNamespace(
+		   ownerDomain = "helloworld.example.com",
+		   ownerName = "helloworld.example.com",
+		   packagePath = "")
+     )
+
+public class TinyGramEndPoint {
+    
+}
